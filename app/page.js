f924e27a9b024202e68553c281be2bf9e1be964d
shakiralam789 <shakiralam1176@gmail.com>
2025-02-09 20:24:21 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,5 @@
+import CombineSales from "@/pages/combineSales/combineSales";
+
+export default function Home() {
+  return <CombineSales />;
+}
---
+++ pages/combineSales/combineSales.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Preview from "./partials/preview";
+
+export default function CombineSales() {
+  return (
+    <div className="h-full container">
+      <div className="py-16 flex flex-wrap gap-4">
+        <Preview className="w-[550px]"/>
+        <div className="flex-1">lorem soifdf</div>
+      </div>
+    </div>
+  );
+}
